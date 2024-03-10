2b5ddf548aa7e8ddd84b0a62362771c36c68bdf8
jirachanon <110511312+jirachanon@users.noreply.github.com>
2024-03-11 01:10:46 +0700

    add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Home from './components/Home.jsx';
+import Login from './components/Login.jsx';
+import Register from './components/Register.jsx';
+import SendBP from './components/SendBP.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/Login",
+    element: <Login />
+  },
+  {
+    path: "/Register",
+    element: <Register />
+  },
+  {
+    path: "/SendBP",
+    element: <SendBP />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
