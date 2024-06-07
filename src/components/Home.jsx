f5ab28d06181f193d978258eccd48f2798aba272
jirachanon <110511312+jirachanon@users.noreply.github.com>
2024-06-07 10:30:10 +0700

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ function Home() {
           title: "กรุณาเข้าสู่ระบบอีกครั้ง",
           confirmButtonText: "ตกลง",
         }).then(() => {
-          window.location.href('https://liff.line.me/2004489610-aP6ng65X')
+          window.location.href = 'https://liff.line.me/2004489610-aP6ng65X';
         });
       }
     })
