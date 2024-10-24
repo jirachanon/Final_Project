a702ce111e50b1d9ea989cc188c8a12e58d46c5a
jirachanon <110511312+jirachanon@users.noreply.github.com>
2024-10-24 11:45:28 +0700

    Debugging Image 6

--- src/components/SendBP.jsx
+++ src/components/SendBP.jsx
@@ -7,7 +7,7 @@ import Cookies from 'js-cookie'
 import ReactCrop, { centerCrop, convertToPixelCrop, makeAspectCrop } from 'react-image-crop'
 import Modal from 'react-modal'
 import cross from '../assets/icons/cross.svg'
-// import setCanvasPreview from './setCanvasPreview'
+import setCanvasPreview from './setCanvasPreview'
 import ex1 from '../assets/IMG/ex1.jpg'
 
 Modal.setAppElement('#root');
@@ -231,51 +231,6 @@ function SendBP() {
             error.pul = "กรุณากรอกข้อมูล!";
         }
         return error;
-    }
-
-    const setCanvasPreview = (
-        image, // HTMLImageElement
-        canvas, // HTMLCanvasElement
-        crop // PixelCrop
-      ) => {
-        const ctx = canvas.getContext("2d");
-        if (!ctx) {
-          throw new Error("No 2d context");
-        }
-      
-        // devicePixelRatio slightly increases sharpness on retina devices
-        // at the expense of slightly slower render times and needing to
-        // size the image back down if you want to download/upload and be
-        // true to the images natural size.
-        const pixelRatio = window.devicePixelRatio;
-        const scaleX = image.naturalWidth / image.width;
-        const scaleY = image.naturalHeight / image.height;
-      
-        canvas.width = Math.floor(crop.width * scaleX * pixelRatio);
-        canvas.height = Math.floor(crop.height * scaleY * pixelRatio);
-      
-        ctx.scale(pixelRatio, pixelRatio);
-        ctx.imageSmoothingQuality = "high";
-        ctx.save();
-      
-        const cropX = crop.x * scaleX;
-        const cropY = crop.y * scaleY;
-      
-        // Move the crop origin to the canvas origin (0,0)
-        ctx.translate(-cropX, -cropY);
-        ctx.drawImage(
-          image,
-          0,
-          0,
-          image.naturalWidth,
-          image.naturalHeight,
-          0,
-          0,
-          image.naturalWidth,
-          image.naturalHeight
-        );
-      
-        ctx.restore();
     }
 
     return (
